f363747f198a7005d80ca84f0d8bfa84d12a66f6
NielsPoldervaart <Niels@meghira.nl>
2021-12-14 16:37:00 +0100

    temp

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <ScrollProgress />
-      {/* <BackToTop /> */}
+      <BackToTop />
 
       <div className='Nav'>
         <NavElementRight />
--- portfolio-website/src/components/BackToTop/BackToTop.js
+++ portfolio-website/src/components/BackToTop/BackToTop.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { IoIosArrowUp } from 'react-icons/io';
 // import CircleType from `circletype`;
 import './BackToTop.css';
 
 const BackToTop = () => {
+
+    useEffect(() => {
+
+        const CircleText = document.querySelector('.CircleText span');
+        CircleText.innerHTML = CircleText.innerText.split("").map(
+            (char, i) => `<span style="transform:rotate(${i * 10.2}deg)">${char}</span>`
+        ).join("");
+        // let BackToTopHandler = () => {
+        // }
+
+        // let WrapperHandler = () => {
+        //   const WrapperUpper = document.getElementById('WrapperTextUpperProjects');
+        //   const WrapperLower = document.getElementById('WrapperTextLowerProjects');
+      
+        //   WrapperUpper.style.transform = `translateX(${window.scrollY / 32}%)`;
+        //   WrapperLower.style.transform = `translateX(-${window.scrollY / 32}%)`;
+        // }
+    
+        // window.addEventListener('scroll', WrapperHandler);
+    
+        // return () => window.removeEventListener('scroll', WrapperHandler);
+      });
+
     return (
         <div className='BackTopButton'>
             <div className='CircleText'>
-                <span>Back to top • Back to top • Back to top • Back to top • </span>
+                <span><span className='CircleTextDark'>Back to top</span><span>Back to top</span><span className='CircleTextDark'>Back to top</span><span>Back to top</span></span>
             </div>
             <div className='Arrow'>
                 <IoIosArrowUp title='Back to top!' />
